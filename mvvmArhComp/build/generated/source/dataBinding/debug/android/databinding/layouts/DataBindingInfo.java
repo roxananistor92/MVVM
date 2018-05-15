e48c42b9b02b6979f9b0ed18fc43f567503a3480
Roxana Nistor <Roxana.Nistor@iquestgroup.com>
2018-05-15 13:09:02 +0300

    Added hockeyApp gradle dependency

--- mvvmArhComp/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="479ff87e-f8a1-4614-8d4f-1d5e9f6d03ff")
-public class DataBindingInfo {}
---
+++ mvvmArhComp/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="80f861c4-1d4d-42e4-84f0-2630ed63e9ec")
+public class DataBindingInfo {}
