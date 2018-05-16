dcc58a9a04f28863a443a27c9d70301dd73918cd
Roxana Nistor <Roxana.Nistor@iquestgroup.com>
2018-05-16 10:23:47 +0300

    Added increase version code functionality

---
+++ mvvmArhComp/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="00457b13-38c0-4c3e-8909-6fc0dbe56da8")
+public class DataBindingInfo {}
--- mvvmArhComp/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="80f861c4-1d4d-42e4-84f0-2630ed63e9ec")
-public class DataBindingInfo {}
